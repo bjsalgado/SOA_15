a8c6eb5c26f917a5ad9b8287ba5545406dbe746d
bjsalgado <bayardo_salgado@yahoo.com>
2015-09-24 23:41:17 +0800

    Fist version not tested of FizzBuzz game

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,22 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(size, &block)
+  arr=[]  
+  1.upto size do |number|
+    case
+    when number % 3 == 0 && number % 5 == 0
+      arr << "FizzBuzz"
+    when number % 3 == 0
+      arr << "Fizz"
+    when number % 5 == 0
+      arr << "Buzz"
+    else
+      arr << number
+    end
+  end
+  if block
+    arr.each { |a| yield a}
+  else
+    arr
+  end
+end
